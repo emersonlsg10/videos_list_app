e208abe4284a10d93ff4afb74c2ea13276083e85
emerson <emersonlsg10@gmail.com>
2022-02-16 11:34:16 -0300

    Fix: criado modal

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,12 +1,95 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  Modal,
+  Pressable,
+} from 'react-native';
+import {useVideos} from '../../hooks/videos';
+import {styles} from './styles';
+import moment from 'moment';
 
 const Home = () => {
+  const {getVideos, videos} = useVideos();
+
+  useEffect(() => {
+    // executa assim que a tela monta/carrega.
+    getVideos({page: 1});
+  }, []);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const handleClick = item => {
+    setSelectedMovie(item);
+    setModalVisible(true);
+  };
+
+  const formatDate = date => moment(date).format('DD-MM-YYYY');
+
   return (
-    <View>
-      <Text>Teste</Text>
-    </View>
+    <>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+        {videos.map((item, index) => (
+          <TouchableOpacity
+            onPress={() => handleClick(item)}
+            style={styles.card}
+            key={index}>
+            <View style={styles.left}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.date}>{formatDate(item.release_date)}</Text>
+              <Text numberOfLines={3} style={styles.overview}>
+                {item.overview}
+              </Text>
+            </View>
+            <View style={styles.right}>
+              <Image
+                resizeMode="contain"
+                style={styles.image_movie}
+                source={{
+                  uri: `http://image.tmdb.org/t/p/w185${item?.poster_path}`,
+                }}
+              />
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>{selectedMovie?.title}</Text>
+              <Image
+                resizeMode="contain"
+                style={styles.image_movie}
+                source={{
+                  uri: `http://image.tmdb.org/t/p/w185${selectedMovie?.poster_path}`,
+                }}
+              />
+              <Text style={styles.date}>
+                {formatDate(selectedMovie?.release_date)}
+              </Text>
+              <Text style={styles.overview}>{selectedMovie?.overview}</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </>
   );
 };
 
--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -2,4 +2,81 @@ import {StyleSheet} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import appMetrics from '../../utils/appMetrics';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    marginTop: 35,
+    marginHorizontal: 10,
+  },
+
+  card: {
+    height: 120,
+    backgroundColor: '#cecece',
+    borderRadius: 15,
+    marginBottom: 15,
+    padding: 10,
+    flexDirection: 'row',
+  },
+
+  title: {
+    fontSize: 16,
+    fontWeight: '700',
+  },
+
+  left: {
+    flex: 1,
+  },
+
+  right: {
+    width: 70,
+  },
+
+  image_movie: {
+    width: 80,
+    height: 80,
+    borderRadius: 5,
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    height: appMetrics.DEVICE_HEIGHT * 0.8,
+    width: appMetrics.DEVICE_WIDTH * 0.9,
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
